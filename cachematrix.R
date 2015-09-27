19727204c5a51b435fcf23ea35712895dad7a0e8
Manni Truong <manni.truong@woodmac.com>
2015-09-27 20:28:47 +0100

    implement get of makeCacheMatrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,23 @@
+## Manni Truong, WoodMac 2015
+
 ## Put comments here that give an overall description of what your
 ## functions do
 
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+    
+    m <- NULL
+    set <- function(y) {
+        
+        x <<- y
+        m <<- NULL
+        
+    }
+    get <- function() x # just returns the matrix
+    
+    list(get = get)
+    
 
 }
 
